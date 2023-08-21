d6a3bf5d28b800fd9907444df39745c23d3b784e
Amastina <nnantaa@gmail.com>
2023-08-20 20:46:53 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,9 +13,12 @@ void more_numbers(void)
 
 	for (i = 1; i <= 10; i++)
 	{
-		if (j >= 10)
+		for (j = 0; j <= 14; j++)
+		{
+			if (j >= 10)
 			_putchar('1');
-		_putchar(j % 10);
+			_putchar(j % 10 + '0');
+		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
