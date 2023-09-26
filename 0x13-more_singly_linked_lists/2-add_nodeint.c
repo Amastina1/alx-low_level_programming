0c57f83d31e89eb0efd489816a2b9e5fa9b37d14
Amastina1 <nnantaa@gmail.com>
2023-09-25 18:36:53 -0700

    3-add_nodeint_end.c

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -2,12 +2,12 @@
 #include <stdio.h>
 
 /**
- * add_nodeint - adds a new node at the beginning
- * of listint_t list
+ * add_nodeint - adds a new node at the beginning of listint_t list
  * @h: head of a double pointer
  * @n: int for the new node
  *
- * Return: the address of the new element or NULL if it failed
+ * Return: the address of the new element or
+ * NULL if it failed
  */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
